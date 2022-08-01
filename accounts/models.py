288f641347c0d706040d11cd74345bc01eca0dfa
yosijuki <shirasaki-y@outlook.jp>
2022-08-01 19:24:30 +0900

    CustomUserモデルとスーパーユーザーを作成

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# class User(AbstractUser):
-#     pass
-
-# class FriendShip(models.Model):
-#     pass
+class CustomUser(AbstractUser):
+    pass
