1f915e176e45d866c71550dc1f16834ce0fd2265
yosijuki <shirasaki-y@outlook.jp>
2022-08-02 22:06:49 +0900

    Revert "登録、ログイン、ログアウトページのの簡単な実装"
    This reverts commit 0c88f8f55b9b4c446b22d3829abf1a54886f8e76.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+
 from . import views
 
+<<<<<<< HEAD
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("login/", views.login_view, name="login"),
