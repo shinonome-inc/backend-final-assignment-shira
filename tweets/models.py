e3afafb12f944b6f06bfd8a827b478b7077e05c0
yosijuki <shirasaki-y@outlook.jp>
2022-09-02 01:17:37 +0900

    tweets/modelsの一部を変更、tweet_detail.htmlの追加（記述は未完成）、base.htmlで、ログインの左隣にユーザ名を表示するように変更

--- tweets/models.py
+++ tweets/models.py
@@ -8,3 +8,6 @@ class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
