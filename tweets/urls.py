c073a04f9465262edcf780c18225aaf051bf6cb3
yosijuki <shirasaki-y@outlook.jp>
2022-09-04 03:32:55 +0900

    urlが要件に不一致していた。修正

--- tweets/urls.py
+++ tweets/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "tweets"
 urlpatterns = [
     path("create/", views.tweet_create_view, name="tweet_create"),
-    path("tweets/<int:tweet_id>", views.tweet_detail_view, name="tweet_detail"),
-    path("tweets/<int:tweet_id>/delete", views.tweet_delete_view, name="tweet_delete"),
+    path("<int:tweet_id>", views.tweet_detail_view, name="tweet_detail"),
+    path("<int:tweet_id>/delete", views.tweet_delete_view, name="tweet_delete"),
 ]
