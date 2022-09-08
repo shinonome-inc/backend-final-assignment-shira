9fab0c5faaf67b7a55bb9571c29004af1cfc0a89
YOSIJUKI <49801280+YOSIJUKI@users.noreply.github.com>
2022-09-09 03:49:49 +0900

    Revert "Tweet"

--- welcome/views.py
+++ welcome/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.views.decorators.http import require_GET
-
-from tweets.models import Tweet
 
 
-@require_GET
 def index_view(request):
-    tweet_list = Tweet.objects.all().order_by("created_at")
-    context = {"tweet_list": tweet_list}
-    return render(request, "welcome/index.html", context)
+    return render(request, "welcome/index.html")
