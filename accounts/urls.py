e2b0c55cfec6fd5eb88b71f0b15552cf017f6f2d
yosijuki <shirasaki-y@outlook.jp>
2022-09-20 05:56:09 +0900

    urlを、要件を満たすように細かく修正

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,14 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("signup/", views.signup_view, name="signup"),
-    path("<str:username>/follow", views.follow_view, name="follow"),
-    path("<str:username>/unfollow", views.unfollow_view, name="unfollow"),
-    path("<str:username>/following_list", views.following_list_view, name="following_list"),
-    path("<str:username>/follower_list", views.follower_list_view, name="follower_list"),
+    path("<str:username>/follow/", views.follow_view, name="follow"),
+    path("<str:username>/unfollow/", views.unfollow_view, name="unfollow"),
+    path(
+        "<str:username>/following_list/",
+        views.following_list_view,
+        name="following_list",
+    ),
+    path(
+        "<str:username>/follower_list/", views.follower_list_view, name="follower_list"
+    ),
 ]
--- tweets/urls.py
+++ tweets/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "tweets"
 urlpatterns = [
     path("create/", views.tweet_create_view, name="create"),
-    path("<int:pk>", views.tweet_detail_view, name="detail"),
-    path("<int:pk>/delete", views.tweet_delete_view, name="delete"),
+    path("<int:pk>/", views.tweet_detail_view, name="detail"),
+    path("<int:pk>/delete/", views.tweet_delete_view, name="delete"),
 ]
